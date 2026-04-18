f405112a01daaf74f321bcca33d7ae233f5a4637
krish406 <155498135+krish406@users.noreply.github.com>
2026-04-18 19:05:54 -0400

    added typescript, changed variable names

--- board.js
+++ board.js
@@ -1,20 +1,19 @@
+"use strict";
 let board = document.getElementsByClassName('board')[0];
-
-for(let i = 0; i < 8; i++){
-    let type_i = i % 2;
-    for(let j = 0; j < 8; j++){
-        let type_j = j % 2;
+for (let i = 0; i < 8; i++) {
+    let parity_i = i % 2;
+    for (let j = 0; j < 8; j++) {
+        let parity_j = j % 2;
         const square = document.createElement("div");
         square.style.width = "85px";
         square.style.height = "85px";
-
-        if(type_i == type_j){
-            square.style.backgroundColor = "#660000";
+        square.textContent = `${i},${j}`;
+        if (parity_i == parity_j) {
+            square.style.backgroundColor = "#fffce7";
         }
-        else{
-            square.style.backgroundColor = "#f8e7bb";
+        else {
+            square.style.backgroundColor = "#c8927e";
         }
-        
         board.appendChild(square);
-    } 
+    }
 }
---
+++ board.ts
@@ -0,0 +1,21 @@
+let board: HTMLElement = document.getElementsByClassName('board')[0] as HTMLElement;
+
+for(let i = 0; i < 8; i++){
+    let parity_i = i % 2;
+    for(let j = 0; j < 8; j++){
+        let parity_j = j % 2;
+        const square = document.createElement("div");
+        square.style.width = "85px";
+        square.style.height = "85px";
+        square.textContent = `${i},${j}`;
+
+        if(parity_i == parity_j){
+            square.style.backgroundColor = "#fffce7";
+        }
+        else{
+            square.style.backgroundColor = "#c8927e";
+        }
+        
+        board.appendChild(square);
+    } 
+}
